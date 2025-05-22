ff490a8ac1529147a7fa17f4f08c690d604278f2
Fotovivo <info@fotovivo.de>
2025-05-22 09:29:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('uploadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const output = document.getElementById('output');
+    output.innerHTML = 'QR code generation logic will go here.';
+});
