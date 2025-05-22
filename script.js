fa9b88842716484147afbf0837417747c3ad568b
Fotovivo <info@fotovivo.de>
2025-05-22 10:06:43 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const output = document.getElementById('output');
-    output.innerHTML = 'QR code generation logic will go here.';
-});
